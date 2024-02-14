5b09b30399242718696596efa392f101370b9cf3
Your Name <you@example.com>
2024-02-14 21:07:56 +0100

    litt js

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,9 @@
+let billetter = [];
+function nyBestilling() {
+    let film = document.getElementById("vfilm").value;
+    let antall = document.getElementById("antall").value;
+    let fornavn = document.getElementById("fnavn").value;
+    let etternavn = document.getElementById("enavn").value;
+    let telefonnummer = document.getElementById("telnr").value;
+    let epost = document.getElementById("epost").value;
+}
